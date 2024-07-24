b1cbc7900b6625c3ce267b2c6a35141bde32912e
Anandu Sankar <ananduap74@gmail.com>
2024-07-24 11:53:09 +0530

    07/24

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const origins = [
 ];
 
 app.use(cors({
-    origin: origins,
+    origin: "https://university-data-ui.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
